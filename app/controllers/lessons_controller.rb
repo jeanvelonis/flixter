548cdcf42bc70b6b794fbb2ed62d7bb3bf318e8a
Jean Velonis <jean.velonis@gmail.com>
2019-07-16 00:03:54 +0000

    sections update

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,15 +8,16 @@ class LessonsController < ApplicationController
 
   private
 
-#try to block user from accessing if they are not enrolled
-  def require_authorized_for_current_lesson
-    if !current_user.enrolled_in?(current_lesson.section.course)
-      redirect_to course_path, alert: "You must be enrolled to view this lesson."
-    end 
- end 
-
   helper_method :current_lesson
   def current_lesson
     @current_lession ||= Lesson.find(params[:id])
   end
+
+#try to block user from accessing if they are not enrolled
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You must be enrolled to view this lesson."
+    end 
+ end 
+
 end
